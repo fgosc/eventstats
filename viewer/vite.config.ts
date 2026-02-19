11ad38a62d1e14fcad527361325a889d1f201b6e
max747 <takanori.kojima@gmail.com>
2026-02-19 23:55:55 +0900

    画面下にリビジョンを表示

--- viewer/vite.config.ts
+++ viewer/vite.config.ts
@@ -1,7 +1,13 @@
+import { execSync } from "node:child_process";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
+const gitRevision = execSync("git rev-parse --short HEAD").toString().trim();
 
 export default defineConfig({
   plugins: [react()],
   base: "/eventstats/",
+  define: {
+    __GIT_REVISION__: JSON.stringify(gitRevision),
+  },
 });
